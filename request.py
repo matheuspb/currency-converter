085e1d08e42f83dda5cf433fe4fce4f296c7ad3f
Matheus Bittencourt <matheuspb2005@hotmail.com>
2016-12-28 19:47:28 -0200

    Taking arguments

--- request.py
+++ request.py
@@ -1,7 +1,12 @@
-import urllib.request, json
+#!/usr/bin/env python
 
-response = urllib.request.urlopen("http://api.fixer.io/latest?base=USD")
+import urllib.request, json
+from sys import argv
+
+response = urllib.request.urlopen(
+        "http://api.fixer.io/latest?base=" + argv[1] +
+        "&symbols=" + argv[2])
 
 rates = json.loads(response.read().decode("utf-8"))
 
-print(rates["rates"]["BRL"])
+print(rates)
